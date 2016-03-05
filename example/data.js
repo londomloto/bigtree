b4561e2152ab618ff158e8afcf6559c1d04eac9a
Londomloto <roso.sasongko@gmail.com>
2016-03-05 17:39:14 +0700

    Update data.js

--- example/data.js
+++ example/data.js
@@ -1,3 +1,4 @@
 var data = [
-    {id: 1, text: 'Electronics', left: 1, right: 2, path: '1', level: 0, leaf: 1, expand: 1}
+    {id: 1, text: 'Electronics', left: 1, right: 4, path: '1', level: 0, leaf: 0, expand: 1},
+    {id: 2, text: 'Television', left: 2, right: 3, path: '1/2', level: 1, leaf: 1, expand: 1}
 ];

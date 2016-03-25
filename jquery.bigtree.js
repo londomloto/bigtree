147ae4a7937ae4c32e45c5de3079e96db18ced8c
Londomloto <roso.sasongko@gmail.com>
2016-03-26 05:23:54 +0700

    Update jquery.bigtree.js

--- jquery.bigtree.js
+++ jquery.bigtree.js
@@ -147,7 +147,7 @@
         delay: 30,
 
         // leading & trailing rendered nodes
-        buffer: 10,
+        buffer: 0,
 
         // node markup, can contains templating tags supported by jsRender
         markup: '<div data-id="{{:id}}" class="bt-node bt-hbox">'+

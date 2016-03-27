ec77853df6a30cb8c5ec95e0c73045140263f0d9
Londomloto <roso.sasongko@gmail.com>
2016-03-28 05:24:32 +0700

    Update trash.js

--- plugins/trash.js
+++ plugins/trash.js
@@ -1,1 +1,22 @@
 
+var TrashPlugin = (function(){
+    
+    var Plugin = function(){
+        this.template = '<a class="btn btn-default btn-xs btn-icon" href="#"><i class="fa fa-trash-o></i></a>';
+    };
+    
+    Plugin.prototype = {
+        onInit: function(tree, data) {
+            this.tree = tree;
+            this.data = data;
+        },
+        onRender: function() {
+            var me = this;
+            this.element.on('click', function(){
+                me.tree.remove(me.data);
+            });
+        }
+    }
+    
+    return Plugin;
+}());

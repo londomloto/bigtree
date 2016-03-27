ac8ffc624a0f21533955b66cf9e8853a256f7091
Londomloto <roso.sasongko@gmail.com>
2016-03-28 05:26:51 +0700

    Update trash.js

--- plugins/trash.js
+++ plugins/trash.js
@@ -2,13 +2,10 @@
 var TrashPlugin = (function(){
     
     var Plugin = function(){
-        this.template = '<a class="btn btn-default btn-xs" href="#"><i class="fa fa-trash-o></i>{{:this.text}}</a>';
+        this.template = '<a class="btn btn-default btn-xs" href="#"><i class="fa fa-trash-o></i></a>';
     };
     
     Plugin.prototype = {
-        update: function() {
-            this.text = 'Remove';  
-        },
         onInit: function(tree, data) {
             this.tree = tree;
             this.data = data;
